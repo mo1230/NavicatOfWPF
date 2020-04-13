0f0abd23d434dd98316728d4b66a316d237eda6f
mo1230 <1144898671@qq.com>
2020-04-13 22:51:45 +0800

    添加项目文件。

---
+++ NavicatOfWPF/Interfaces/IUserFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavicatOfWPF.Interfaces
+{
+    interface IUserFunction
+    {
+        bool SignIn(string name, string pwd);       // 注册
+        bool LogIn(string name, string pwd);        // 登录
+
+        bool ConnectToDB(string dbName);            // 链接数据库
+        
+        // 新建表  删除表
+        void NewTable(string table);
+        void DelTable(string table);
+
+        // 查找操作
+        void QueryAll(string table);
+        void QueryData(string table, string condition);
+
+        // 删除操作
+        void DelData(string table, string condition);
+
+        // 添加操作
+        void AddData(string table, string data);
+
+        // 插入操作
+        void InsertData(string table, string data, string conditon);
+    }
+}
